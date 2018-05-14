43cfd163db311ac86df8b516574dfc27e1740ab2
Maxime Spriet <MSpriet@slb.com>
2018-05-14 14:18:57 +0200

    Change db json file, add images repository with some images, update routes for bands to avoid conflict between id and add components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SongsModule } from './songs/songs.module';
 import { MusiciansModule } from './musicians/musicians.module';
+import { BandsAddComponent } from './bands/bands-add/bands-add.component';
+import { BandsLayoutComponent } from './bands/bands-layout/bands-layout.component';
 
 const routes: Routes = [{
     path: '',
     redirectTo: 'bands',
     pathMatch: 'full'
+  },
+  {
+    path: 'bands',
+    component: BandsLayoutComponent,
+  },
+  {
+    path: 'bands/add',
+    component: BandsAddComponent,
   },
   {
     path: 'bands/:id',
@@ -25,10 +35,6 @@ const routes: Routes = [{
       path: '',
       component: BandsShowComponent,
     }]
-  },
-  {
-    path: 'bands',
-    loadChildren: () => BandsModule,
   },
   {
     path: '**',
