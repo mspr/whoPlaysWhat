82057a61fd3edf82ce87033f6c113743db061695
Maxime Spriet <MSpriet@slb.com>
2018-05-18 10:59:10 +0200

    Fix song remove method by considering songIds in bands.

--- src/app/songs/songs-list/songs-list.component.ts
+++ src/app/songs/songs-list/songs-list.component.ts
@@ -6,6 +6,8 @@ import { SongService } from './../../core/song.service';
 import { Song } from './../song';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'wpw-songs-list',
@@ -46,9 +48,26 @@ export class SongsListComponent implements OnInit, OnDestroy
 
   onRemove(id)
   {
-    this.songService.remove(id).subscribe(() => {
+    this.songService.getById(id).switchMap((song) =>
+    {
+      return this.bandService.getAll().switchMap((bands) =>
+      {
+        let updateBandObservables = new Array<Observable<Band>>();
+        bands.forEach(band => {
+          let index = band.songIds.indexOf(song.id);
+          if (index != -1) {
+            band.songIds.splice(band.songIds.indexOf(song.id), 1);
+            updateBandObservables.push(this.bandService.update(band));
+          }
+        });
+
+        return forkJoin(updateBandObservables);
+      })
+      .switchMap(() => this.songService.remove(id))
+    })
+    .subscribe(() => {
       this.songService.removed.emit();
-      this.router.navigate(['songs']);
+      this.router.navigate([`bands/${this.bandId}`, 'songs']);
     });
   }
 
