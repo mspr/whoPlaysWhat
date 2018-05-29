ef147cdc43d729ecfbe49d5b8bbc3e7768bed1e5
Maxime Spriet <MSpriet@slb.com>
2018-05-29 14:22:09 +0200

    Use song structure component in song update component, Do some clean up.

--- src/app/songs/songs-update/songs-update.component.ts
+++ src/app/songs/songs-update/songs-update.component.ts
@@ -4,8 +4,6 @@ import { Tonalities } from './../../core/tonalities.enum';
 import { Component, OnInit } from '@angular/core';
 import { Song } from '../song';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SongParts } from '../../core/song-parts.enum';
-import { MusicianService } from '../../core/musician.service';
 
 @Component({
   selector: 'wpw-songs-update',
@@ -15,18 +13,13 @@ import { MusicianService } from '../../core/musician.service';
 export class SongsUpdateComponent implements OnInit {
 
   public bandId : number;
-  public musicians : Musician[] = new Array<Musician>();
   public song = new Song();
   public tonalities = Object.keys(Tonalities);
   public tonality : Tonalities;
   public tempo;
 
-  public availableParts : string[] = Object.keys(SongParts);
-  public songParts : string[] = [];
-
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
-    private musicianService: MusicianService,
     private songService: SongService) { }
 
   ngOnInit()
@@ -39,11 +32,6 @@ export class SongsUpdateComponent implements OnInit {
       let infoByBand = song.bands.find(band => band.id == this.bandId);
       this.tempo = infoByBand.tempo;
       this.tonality = infoByBand.tonality;
-      this.songParts = this.song.structure;
-    });
-
-    this.musicianService.getAllByBand(this.bandId).subscribe((musicians) => {
-      this.musicians = musicians;
     });
   }
 
@@ -60,51 +48,5 @@ export class SongsUpdateComponent implements OnInit {
 
   private getSongInfoByBand() {
     return this.song.bands.find(band => band.id == this.bandId);
-  }
-
-  getMusicianColor(musician) {
-    return (musician != null) ? musician.bands.find(element => element.id_band == this.bandId).color : null;
-  }
-
-  addSongPart(songPart)
-  {
-    if (songPart === SongParts.Outro) {
-      this.availableParts = [];
-    }
-
-    let introductionIdx = this.availableParts.indexOf(SongParts.Introduction);
-    if (introductionIdx != -1) {
-      this.availableParts.splice(introductionIdx, 1);
-    }
-
-    if (songPart === SongParts.Verse || songPart === SongParts.Solo) {
-      let verseOccurences = this.songParts.filter((value) => {
-        return value.includes(songPart);
-      }).length;
-      songPart = songPart + " " + (verseOccurences + 1);
-    }
-
-    this.songParts.push(songPart);
-  }
-
-  clearSongParts() {
-    this.songParts.length = 0;
-    this.availableParts = Object.keys(SongParts);
-    this.song.musicians = [];
-  }
-
-  doesMusicianPlayThisPart(part, musicianId) {
-    let partsPlayedByTheMusician = this.song.musicians.find(elt=> elt.id == musicianId);
-    return partsPlayedByTheMusician != undefined && partsPlayedByTheMusician.plays.find(elt => elt === part);
-  }
-
-  onClick(musician, part)
-  {
-    let musicianInfo = this.song.musicians.find(info => info.id === musician.id);
-    if (musicianInfo) {
-      musicianInfo.plays.push(part);
-    } else {
-      this.song.musicians.push({id: musician.id, plays:[part]});
-    }
   }
 }
