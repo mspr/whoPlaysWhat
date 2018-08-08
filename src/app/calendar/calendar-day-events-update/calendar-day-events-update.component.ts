0252ff63ec858274430c009e5bd88710b1493e88
Maxime Spriet <MSpriet@slb.com>
2018-08-08 16:44:46 +0200

    Continue to implement the calendar day events update component by adding event frequency.

--- src/app/calendar/calendar-day-events-update/calendar-day-events-update.component.ts
+++ src/app/calendar/calendar-day-events-update/calendar-day-events-update.component.ts
@@ -8,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
 
 export class CalendarDayEventsUpdateComponent implements OnInit
 {
+  public frequencies = ["Once", "Once per month", "Once per week", "Every day"];
+  public frequencyColors = ["rgba(0,0,0,0)", "rgba(0,0,0,0)", "rgba(0,0,0,0)", "rgba(0,0,0,0)"];
+
   constructor()
   {
   }
 
   ngOnInit()
   {
+  }
+
+  updateFrequency(idx: number)
+  {
+    if (idx == 0)
+    {
+      this.frequencyColors = ["rgba(205, 234, 125, 0.5)", "rgba(0,0,0,0)", "rgba(0,0,0,0)", "rgba(0,0,0,0)"];
+    }
+    else if (idx == 1)
+    {
+      this.frequencyColors = ["rgba(255, 234, 125, 0.5)", "rgba(255, 234, 125, 0.5)", "rgba(0,0,0,0)", "rgba(0,0,0,0)"];
+    }
+    else if (idx == 2)
+    {
+      this.frequencyColors = ["rgba(242, 172, 46, 0.5)", "rgba(242, 172, 46, 0.5)", "rgba(242, 172, 46, 0.5)", "rgba(0,0,0,0)"];
+    }
+    else if (idx == 3)
+    {
+      this.frequencyColors = ["rgba(255, 84, 46, 0.5)", "rgba(255, 84, 46, 0.5)", "rgba(255, 84, 46, 0.5)", "rgba(255, 84, 46, 0.5)"];
+    }
   }
 }
