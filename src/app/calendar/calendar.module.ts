54e5b856614d2d8a85d63dd0811db2354c76feaf
Maxime Spriet <MSpriet@slb.com>
2018-08-09 11:07:04 +0200

    Continue to implement calendar day events update component with a form.

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -5,10 +5,12 @@ import { CalendarShowComponent } from './calendar-show/calendar-show.component';
 import { CalendarDayEventsUpdateComponent } from './calendar-day-events-update/calendar-day-events-update.component';
 import { CalendarDayEventsOverviewComponent } from './calendar-day-events-overview/calendar-day-events-overview.component';
 import { CalendarDayEventsShowComponent } from './calendar-day-events-show/calendar-day-events-show.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     CalendarShowComponent,
