25919ddce40629bd16070e342e2b56dff8745ff2
Maxime Spriet <MSpriet@slb.com>
2018-09-20 14:54:18 +0200

    Continue to implement the authentication service with methods and local storage for id token and expires at information.

--- localServer.js
+++ localServer.js
@@ -3,11 +3,38 @@ var app = express()
 
 let bands = ["The Beatles", "Dire Straits"];
 
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  console.log('Hello');
+});
+
 app.get('/bands', function (req, res) {
   console.log(bands);
   res.json(bands);
-})
+});
+
+app.get('/login', function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (validateEmailAndPassword()) {
+    const userId = findUserIdForEmail(email);
+    const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+      algorithm: 'RS256',
+      expiresIn: 120,
+      subject: userId
+    });
+
+    res.status(200).json({
+      idToken: jwtBearerToken
+    });
+  }
+  else {
+    res.sendStatus(401);
+  }
+});
 
 app.listen(8080, function() {
-    console.log('serving...');
-})
+  console.log('serving...');
+});
--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import * as moment from "moment";
 
 export class User
 {
@@ -15,6 +16,38 @@ export class AuthenticationService
 
   login(email : string, password : string)
   {
-    return this.httpClient.post<User>(environment.baseUrl + '/login', {email, password}).shareReplay();
+    return this.httpClient.post<User>(environment.baseUrl + '/login', {email, password})
+      .do(res => this.setSession)
+      .shareReplay();
+  }
+
+  logout()
+  {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  private setSession(authResult)
+  {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+  }
+
+  public isLoggedIn()
+  {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut()
+  {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration()
+  {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
   }
 }
