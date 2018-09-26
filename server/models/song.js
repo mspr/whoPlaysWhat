e1fda9f6e6188b524b74226dec2c8a5b6e8b1cc0
Maxime Spriet <MSpriet@slb.com>
2018-09-26 16:21:50 +0200

    Continue to implement Song mongoose schema for node server.

--- server/models/song.js
+++ server/models/song.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 let SongSchema = new mongoose.Schema({
-  title: { type: String, required: [true, "can't be blank"], index: true}
+  title: { type: String, required: [true, "can't be blank"], index: true},
+  tempo: { type: Number },
+  level: { type: String, enum: ['very simple', 'simple', 'advanced', 'hard', 'evil'] },
+  progression: { type: Number, min: 0, max: 100 },
+  tonality: { type: String, enum: ['A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#'] }
 });
 
 SongSchema.methods.toDto = function () {
   return {
     id: this._id,
-    title: this.title
+    title: this.title,
+    tempo: this.tempo,
+    level: this.evil,
+    progression: this.progression,
+    tonality: this.tonality
   }
 }
 
